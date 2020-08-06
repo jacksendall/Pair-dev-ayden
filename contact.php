b590e176c82b3ad44c757eb3af4411ba78a3ab61
jacksendall <jacksendall@gmail.com>
2020-08-06 13:23:43 +0100

    init

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+    include 'header.php';
+?>
+
+
+<div class="container">
+    <div class="formwrapper">
+        <form method="POST" action="contact.inc.php">
+            <div class="formflex">
+            <input role="name" class="col-6" type="text" name="name" placeholder="Your Name">
+            <input role="email" class="col-6" type="email" name="mailid" placeholder="Email Address">
+            </div>
+
+            <textarea class="col-12" name="message" placeholder="Enter Message Here"></textarea>
+            <input class="btn btn-success" type="button" value="Submit" name="sbmt-btn">
+
+            
+            
+            
+        </form>
+    </div>
+</div>
+
+<?php
+    include 'footer.php';
+?>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="JAck & Ayden's Pair Project">
+    <meta name="keywords" content="HTML,CSS,Reflection,Netmatters,Pair Project">
+    <meta name="author" content="Jack Sendall, Ayden Cole">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Sharon Wray</title>
+</head>
+<body>
+<div class="content-wrapper">
+    <div class="header-wrapper">
+        <div class="container main-header">
+
+
+        <div class="brand">
+            <h1 class="border-btm">Sharon Wray</h1>
+            <h3>Accountancy Services</h3>
+        </div>
+
+
+        <div class="actions">
+            <button class="btn navbutton">My Services</button>
+            <button class="btn navbutton"> Switch Accountants</button>
+            <button class="btn navbutton">FAQ's</button>
+            <button class="btn navbutton">Contact Us</button>
+            <button class="btn navbutton"><strong>01953 687077</strong></button>
+        </div>
+    </div>
+</div>
+    
