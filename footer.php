724ee650d59f3bf91f4ed2a1e907e4cd14294ecd
Ayden-scs <68744963+Ayden-scs@users.noreply.github.com>
2020-08-06 16:44:06 +0100

    End of day commit

--- footer.php
+++ footer.php
@@ -1,3 +1,27 @@
-
+    <footer>
+        <div class="container footer-wrapper">
+            <div>
+                <img src="images/icons.png" alt="">
+                <p>AAT licensed accountant with over 15 years of accountancy experience.</p>
+                <p>My services included bookkeeping, full accounts production, payroll, VAT and tax.</p>
+            </div>
+            <div>
+                <h2>Contact Us</h2>
+                <ul>
+                    <li></li>
+                    <li>Sharon Wray Accountancy services</li>
+                    <li>Unit 4 Silver Farm</li>
+                    <li>Silver Street</li>
+                    <li>Besthorpe</li>
+                    <li>Norfolk, NR17 2NY</li>
+                    <li>Tel: </li>
+                </ul>
+            </div>
+            <div>  
+                <h2>Sections</h2>   
+            </div>    
+        </div>
+    </footer>
+</div>
 </body>
 </html>
